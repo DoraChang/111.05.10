ff8704456ca657129a64c566e8206c0b5fe31e2e
DoraChang <104436657+DoraChang@users.noreply.github.com>
2022-05-10 21:15:44 +0800

    ex1-3.cpp
    x+y
    Good night!

---
+++ ex1-3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x,y;
+    printf("please input x: \n");
+    scanf("%d",&x);
+    
+    printf("please input y: \n");
+    scanf("%d",&y);
+        
+    printf(" %d + %d =%d \n",x,y,x+y);
+    
+    cout<<"Good night!";
+    return 0;
+}
