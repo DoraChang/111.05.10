a73f3701fd5109dadc728b88c826db9c59bd2d31
DoraChang <104436657+DoraChang@users.noreply.github.com>
2022-05-10 21:40:22 +0800

    ex1-3.cpp
    x+y(運算元)
    Good night!

--- ex1-3.cpp
+++ ex1-3.cpp
@@ -11,10 +11,22 @@ int main()
     scanf("%d",&y);
     /*
     + - * / %
+    
+    ++X
+    X++
+    --X
+    X--
+    
+    x=16
+    x >> 1
+    x >> 2
+    x << 1
+    
     */
     
-    printf(" %d + %d =%d \n",x,y,x+y);
     
+    printf(" %d + %d =%d \n",x,y,x+y);
+    printf(" %d + %d =%d \n",--x,y,x+y);
     cout<<"Good night!";
     return 0;
 }
